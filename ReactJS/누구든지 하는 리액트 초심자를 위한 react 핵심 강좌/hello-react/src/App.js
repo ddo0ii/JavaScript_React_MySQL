3eeb9b4adedd68338564f3677b53d09478cfb3cb
ddo0ii <soyeong5857@gmail.com>
2023-04-07 15:04:37 +0900

    배열에 데이터 삽입하기

--- ReactJS/누구든지 하는 리액트 초심자를 위한 react 핵심 강좌/hello-react/src/App.js
+++ ReactJS/누구든지 하는 리액트 초심자를 위한 react 핵심 강좌/hello-react/src/App.js
@@ -1,12 +1,39 @@
-import "./App.css";
+import React, { Component } from "react";
 import PhoneForm from "./components/PhoneForm";
 
-function App() {
-  return (
-    <div className="App">
-      <PhoneForm />
-    </div>
-  );
+class App extends Component {
+  id = 0;
+
+  state = {
+    information: [],
+  };
+  // push가 아닌 concat으로 불변성을 지켜줘야한다
+  // handleCreate = (data) => {
+  //   this.setState({
+  //     information: this.state.information.concat(data),
+  //   });
+  // };
+  // handleCreate를 조금 더 간단하게 표현해보자
+  handleCreate = (data) => {
+    const { information } = this.state;
+    // 비구조화 할당을 사용하면 아래와 같이 더 짧은 코드를 구현도 가능하다
+    // id는 고유한 값 넣기
+    this.setState({
+      information: information.concat(
+        Object.assign({}, data, {
+          id: this.id++,
+        })
+      ),
+    });
+  };
+  render() {
+    return (
+      <div>
+        <PhoneForm onCreate={this.handleCreate} />
+        {JSON.stringify(this.state.information)}
+      </div>
+    );
+  }
 }
 
 export default App;
--- ReactJS/누구든지 하는 리액트 초심자를 위한 react 핵심 강좌/hello-react/src/components/PhoneForm.js
+++ ReactJS/누구든지 하는 리액트 초심자를 위한 react 핵심 강좌/hello-react/src/components/PhoneForm.js
@@ -12,9 +12,26 @@ class PhoneForm extends Component {
     });
   };
 
+  // 등록 버튼 눌러서, 새로고침 되는 것을 방지하기 위해
+  //   페이지가 리더링 되는것을 방지하기 위해서
+  //   e.preventDefault(); 사용 >> 원래 해야하는 것 안하는 것 >> 값 넣고, 새로고침 해야하는데 안하는 것
+  handleSubmit = (e) => {
+    e.preventDefault();
+    // this.props.onCreate({
+    //   name: this.state.name,
+    //   phone: this.state.phone,
+    // });
+    // 또는 아래와 같이
+    this.props.onCreate(this.state);
+    // input 입력 값 초기화
+    this.setState({
+      name: "",
+      phone: "",
+    });
+  };
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <input
           name="name"
           placeholder="이름"
@@ -27,9 +44,7 @@ class PhoneForm extends Component {
           onChange={this.handleChange}
           value={this.state.phone}
         />
-        <div>
-          {this.state.name} {this.state.phone}
-        </div>
+        <button type="submit">등록</button>
       </form>
     );
   }

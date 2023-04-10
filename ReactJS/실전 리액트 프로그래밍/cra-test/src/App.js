87d9800e88eafac15e3b4650228dd2797428d314
ddo0ii <soyeong5857@gmail.com>
2023-04-10 17:54:38 +0900

    useMount도 훅 만들어보기

--- ReactJS/실전 리액트 프로그래밍/cra-test/src/App.js
+++ ReactJS/실전 리액트 프로그래밍/cra-test/src/App.js
@@ -1,9 +1,11 @@
 import UseEffectAPI from "./component/UseEffectAPI";
 import UseEffectEvent from "./component/UseEffectEvent";
+import useMount from "./hooks/useMount";
 
 function App() {
   return (
     <div className="App">
+      <useMount />
       <UseEffectEvent />
       <UseEffectAPI />
     </div>

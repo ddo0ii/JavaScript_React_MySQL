45732fb7767bc5aaf8224fb88c68b62bcf305079
ddo0ii <soyeong5857@gmail.com>
2023-04-10 17:02:25 +0900

    기본 설치

---
+++ ReactJS/실전 리액트 프로그래밍/cra-test/src/App.js
@@ -0,0 +1,13 @@
+import UseEffectAPI from "./component/UseEffectAPI";
+import UseEffectEvent from "./component/UseEffectEvent";
+
+function App() {
+  return (
+    <div className="App">
+      <UseEffectEvent />
+      <UseEffectAPI />
+    </div>
+  );
+}
+
+export default App;

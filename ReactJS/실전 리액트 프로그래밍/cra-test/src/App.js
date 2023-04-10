a897e78677296121e7c43f783af6c64c648ab559
ddo0ii <soyeong5857@gmail.com>
2023-04-10 17:03:26 +0900

    useEffect

--- ReactJS/실전 리액트 프로그래밍/cra-test/src/App.js
+++ ReactJS/실전 리액트 프로그래밍/cra-test/src/App.js
@@ -1,5 +1,13 @@
+import UseEffectAPI from "./component/UseEffectAPI";
+import UseEffectEvent from "./component/UseEffectEvent";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <UseEffectEvent />
+      <UseEffectAPI />
+    </div>
+  );
 }
 
 export default App;

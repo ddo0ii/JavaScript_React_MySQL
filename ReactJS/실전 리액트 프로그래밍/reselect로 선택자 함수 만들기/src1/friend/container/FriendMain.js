88d340c65e1bcd95aac844354a46a04a8e2f316f
ddo0ii <soyeong5857@gmail.com>
2023-04-13 16:38:19 +0900

    reselect로 선택자 함수 만들기

---
+++ ReactJS/실전 리액트 프로그래밍/reselect로 선택자 함수 만들기/src1/friend/container/FriendMain.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { getNextFriend } from "../../common/mockData";
+import { addFriend, setAgeLimit, setShowLimit } from "../state";
+import FriendList from "../component/FriendList";
+import NumberSelect from "../component/NumberSelect";
+import { useSelector, useDispatch, shallowEqual } from "react-redux";
+import { MAX_AGE_LIMIT, MAX_SHOW_LIMIT } from "../common";
+
+export default function FriendMain() {
+  const [ageLimit, showLimit, friendsWithAgeLimit, friendsWithAgeShowLimit] =
+    useSelector((state) => {
+      const { friends, ageLimit, showLimit } = state.friend;
+      const friendsWithAgeLimit = friends.filter(
+        (friend) => friend.age <= ageLimit
+      );
+      return [
+        ageLimit,
+        showLimit,
+        friendsWithAgeLimit,
+        friendsWithAgeLimit.slice(0, showLimit),
+      ];
+    }, shallowEqual);
+  const dispatch = useDispatch();
+  function onAdd() {
+    const friend = getNextFriend();
+    dispatch(addFriend(friend));
+  }
+  console.log("FriendMain render");
+  return (
+    <div>
+      <button onClick={onAdd}>친구 추가</button>
+      <NumberSelect
+        onChange={(v) => dispatch(setAgeLimit(v))}
+        value={ageLimit}
+        options={AGE_LIMIT_OPTIONS}
+        postfix="세 이하만 보기"
+      />
+      <FriendList friends={friendsWithAgeLimit} />
+      <NumberSelect
+        onChange={(v) => dispatch(setShowLimit(v))}
+        value={showLimit}
+        options={SHOW_LIMIT_OPTIONS}
+        postfix="명 이하만 보기 (연령 제한 적용)"
+      />
+      <FriendList friends={friendsWithAgeShowLimit} />
+    </div>
+  );
+}
+
+const AGE_LIMIT_OPTIONS = [15, 20, 25, MAX_AGE_LIMIT];
+const SHOW_LIMIT_OPTIONS = [2, 4, 6, MAX_SHOW_LIMIT];

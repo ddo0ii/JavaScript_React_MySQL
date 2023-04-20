efccee7be6c9c2a2bcd4f4ff854c8a8bca0c2083
ddo0ii <soyeong5857@gmail.com>
2023-04-20 17:08:17 +0900

    단순 식당 목록 만들어보기

--- MySQL/mysql-web-practice/database/sql.js
+++ MySQL/mysql-web-practice/database/sql.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool(
     host: "localhost",
     user: "root",
     database: "mydatabase",
-    password: "~~",
+    password: "--",
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
@@ -18,6 +18,24 @@ const sql = {
     const [rows] = await promisePool.query(`
       SELECT * FROM sections
     `);
+    return rows;
+  },
+
+  getBusinessesJoined: async (query) => {
+    const sqlQuery = `
+      SELECT * FROM sections S
+      LEFT JOIN businesses B
+        ON S.section_id = B.fk_section_id
+      WHERE TRUE
+        ${query.section ? "AND section_id = " + query.section : ""}
+        ${query.floor ? "AND floor = " + query.floor : ""}
+        ${query.status ? "AND status = '" + query.status + "'" : ""}
+      ORDER BY
+         ${query.order ? query.order : "business_id"}
+    `;
+    console.log(sqlQuery);
+
+    const [rows] = await promisePool.query(sqlQuery);
     return rows;
   },
 };
